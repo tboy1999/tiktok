41de13601f196b0b5f1cd76c31ece0ceabef5c69
tongtai <meotai1999@gmail.com>
2022-05-25 15:58:49 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useRef } from "react";
+
+const initial = {
+  job: "",
+  jobs: []
+}
+const SET_JOB = "set_job"
+const ADD_JOB = "add_job"
+const DELETE_JOB = "delete_job"
+const setJob = payload => {
+  return {
+    type: SET_JOB,
+    payload
+  }
+}
+const addJob = payload => {
+  return {
+    type: ADD_JOB,
+    payload
+  }
+}
+const deleteJob = payload => {
+  return {
+    type: DELETE_JOB,
+    payload
+  }
+}
+const reduce = (state, action) => {
+  switch (action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        job: action.payload
+      }
+    case ADD_JOB:
+      return {
+        ...state,
+        jobs: [...state.jobs, action.payload]
+      }
+    case DELETE_JOB:
+      const newJobs = [...state.jobs]
+      newJobs.splice(action.payload, 1)
+      return {
+        ...state,
+        jobs: newJobs
+      }
+  
+    default:
+      throw new Error(`Invalid action ${action.type}`)
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reduce, initial)
+  const {job, jobs} = state
+  const handleAddJob = () => {
+    dispatch(addJob(job))
+    dispatch(setJob(""))
+
+    inputRef.current.focus()
+  }
+  const inputRef = useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding : 20}}>
+      <input type="text" value={job} onChange={(e) => dispatch(setJob(e.target.value))} ref={inputRef}/>
+      <button onClick={handleAddJob}>add</button>
+      <ul>
+        {jobs.map((job, index) => <li key={index}>{job} <span onClick={() => dispatch(deleteJob(index))}>&times;</span></li>)}
+      </ul>
     </div>
   );
 }
